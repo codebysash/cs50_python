1f841f5a6356b5801527b73e7335a5193859013f
sashshetty <95724242+codebysash@users.noreply.github.com>
2025-07-27 13:58:49 +1000

    Add Lecture 1 project parity.py and update README

--- lecture1_conditions/parity.py
+++ lecture1_conditions/parity.py
@@ -1,9 +1,14 @@
 def main():
-    x = int(input("What is x?: "))
+    try:
+        x = int(input("What is x?: "))
+    except ValueError:
+        print("Please enter valid number.")
+        return
+
     if is_even(x):
-        print("Even")
+        print("The number you have chosen is Even.")
     else:
-        print("Odd")
+        print("The number you have chosen is Odd.")
 
 def is_even(n):
     return True if n % 2 == 0 else False
