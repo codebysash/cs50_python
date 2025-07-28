1faec44041fa430853884a19226707fed2b44d29
sashshetty <95724242+codebysash@users.noreply.github.com>
2025-07-28 18:32:08 +1000

    Finalize guessing game with play again feature

--- lecture2_loops/number_guessing.py
+++ lecture2_loops/number_guessing.py
@@ -1,23 +1,30 @@
 import random
 
-number = random.randint(1, 100)
-
-attempts = 0
+def play_game():
+    number = random.randint(1, 100)
+    attempts = 0
+    while True:
+        guess_input = input("Guess a number between 1 and 100: ")
+        try:
+            guess = int(guess_input)
+        except ValueError:
+            print("That's not a number! Pleae enter number between 1 and 100.")
+            continue
+        attempts += 1
+        if attempts > 7:
+            print("You've reached the maximum number of attempts. You lose!")
+            break
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print("Correct! You guessed the number")
+            break
+    
 while True:
-    guess_input = input("Guess a number between 1 and 100: ")
-    try:
-        guess = int(guess_input)
-    except ValueError:
-        print("That's not a number! Pleae enter number between 1 and 100.")
-        continue
-    attempts += 1
-    if attempts > 7:
-        print("You've reached the maximum number of attempts. You lose!")
-        break
-    if guess < number:
-        print("Too low!")
-    elif guess > number:
-        print("Too high!")
-    else:
-        print("Correct! You guessed the number")
+    play_game()
+    play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+    if play_again != "yes":
+        print("Thanks for playing")
         break
